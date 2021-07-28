2f9c57a5752d8d45190f3c2ed80722931723f8e1
George <gbridge@ncsu.edu>
2021-07-28 18:19:13 -0400

    added image and links and filled out about tab

--- FinalProject/ui.R
+++ FinalProject/ui.R
@@ -1,40 +1,31 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(shinydashboard)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+# using shinydashboard for some nice features and to accommodate multiple pages
+shinyUI(dashboardPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            tabsetPanel(
-             tabpanel("About",plotOutput("distPlot")),
-             tabpanel("Data"),
-             tabpanel("Data Exploration"),
-             tabpanel("Modeling")
-            )
-            
+    dashboardHeader(title = "Predicting Home Prices", titleWidth = 350),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("About", tabName = "about"),
+            menuItem("Data", tabName = "data"),
+            menuItem("Data Exploration", tabName = "dexp"),
+            menuItem("Modeling", tabName = "modeling")
+                    )
+                     ),
+    
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "about",
+                    h4("This app is built to allow for exploration, subsetting, modeling and predicting with a dataset of home prices and other information associated with each listing such as lot size and number of rooms."),
+                    h4("The dataset comes from an advanced regression competition put on by Kaggle, more information can be found here:"),
+                    h4(tags$a(href = "https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data?select=test.csv", "housing data")),
+                    h4("The purpose of the about tab is to give an overview of what the app does and some information about the data. The purpose of the Data tab is to allow the user to scroll through the data, subset it and save the data to a file. The purpose of the Data Exploration tab is to allow the user to create numerical and graphical summaries of the data. The purpose of the Modeling tab is to allow the user to fit different models to the data, and make a prediction based on certain inputs"),
+                    img(src = "House.jpg", height = 300, width = 300)),
+            tabItem(tabName = "data"),
+            tabItem(tabName = "dexp"),
+            tabItem(tabName = "modeling")
         )
     )
 ))
