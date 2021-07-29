ed6bdac65215d8f42595b8d85cde49dba1ef8548
George <gbridge@ncsu.edu>
2021-07-29 11:45:09 -0400

    forgot to click save on some files

--- FinalProject/server.R
+++ FinalProject/server.R
@@ -8,7 +8,7 @@ library(DT)
 library(dplyr)
 #read in the data and select the columns that I want to use
 data <- read_csv("train.csv")
-data <- data %>% select(c("LotArea", ))
+data <- data %>% select(c("LotArea", "BldgType", "HouseStyle", "OverallQual", "OverallCond", "1stFlrSF", ""))
 #begin server function
 
 shinyServer(function(input, output) {
